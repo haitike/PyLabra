a841bf34c490c367e34c5453d7050ef6bdc0beaf
haitike <haitike@779d064d-a567-7f01-b0a5-fff1745a0c06>
2010-08-13 09:14:50 +0000

    git-svn-id: http://pruebas-dai.googlecode.com/svn/trunk/py-deutsch@62 779d064d-a567-7f01-b0a5-fff1745a0c06

--- FramePrincipal.py
+++
@@ -1,8 +0,0 @@
-import wx
-
-class FramePrincipal(wx.Frame):
-    def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, size=(250, 200))
-
-        self.Show(True)
-
