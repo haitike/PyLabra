71f5e67656d7389b6594675aedf80f4fa99d87ad
haitike <haitike@779d064d-a567-7f01-b0a5-fff1745a0c06>
2010-08-15 15:02:59 +0000

    pruebas con listbox
    git-svn-id: http://pruebas-dai.googlecode.com/svn/trunk/py-deutsch@64 779d064d-a567-7f01-b0a5-fff1745a0c06

--- framePrincipal.py
+++ framePrincipal.py
@@ -3,14 +3,19 @@ from database import BaseDeDatos
 
 class FramePrincipal(wx.Frame):
     def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, size=(250, 200))
+        wx.Frame.__init__(self, parent, id, title, size=(250, 250))
         
         boton1 = wx.Button(self, -1, '+', (10,10))
         boton2 = wx.Button(self, -1, '-', (10,60))
-        self.texto = wx.StaticText(self, -1, '0', (40,110))
+        boton3 = wx.Button(self, -1, 'Meter', (10,110))
+        boton4 = wx.Button(self, -1, 'Quitar', (10,160))
+        self.texto = wx.StaticText(self, -1, '0', (40,210))
+        self.lista = wx.ListBox(self, -1,(100,10),(100,160),["0"], wx.LB_SINGLE)
 
         self.Bind(wx.EVT_BUTTON, self.OnMas, id=boton1.GetId())
         self.Bind(wx.EVT_BUTTON, self.OnMenos, id=boton2.GetId())
+        self.Bind(wx.EVT_BUTTON, self.OnMeter, id=boton3.GetId())
+        self.Bind(wx.EVT_BUTTON, self.OnQuitar, id=boton4.GetId())
 
         self.Show(True)
 
@@ -21,3 +26,12 @@ class FramePrincipal(wx.Frame):
     def OnMenos(self,event):
         valor = int(self.texto.GetLabel()) - 1
         self.texto.SetLabel(str(valor))
+
+    def OnMeter(self,event):
+        self.lista.Insert(self.texto.GetLabel(),self.lista.GetCount())
+
+    def OnQuitar(self,event):
+        if self.lista.GetSelection() >= 0: 
+            self.lista.Delete(self.lista.GetSelection())
+        else:
+             wx.MessageBox('Selecciona el elemento a borrar', 'Error',wx.ICON_ERROR)
